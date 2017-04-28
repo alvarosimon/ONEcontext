3146f2ffec32fd899d75024e2e267e7a1337276e
Álvaro Simón <varosim@gmail.com>
2017-04-28 13:11:58 +0200

    Fix Docker swarm

--- vibcontex.sh
+++ vibcontex.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-CEPH_MOUNT="/cephfs"
+CEPH_MOUNT="/galaxy_base"
 SSH_DIR="/home/docker/.ssh"
 
 # Update the system and install Ceph repos
 yum update -y
 yum install https://download.ceph.com/rpm-jewel/el7/noarch/ceph-release-1-1.el7.noarch.rpm -y
 yum install yum-utils ceph-common -y
+
+# Set hostname
+hostnamectl set-hostname --static $(/sbin/ip -4 -o addr show to 10.145.0.0/16 | /bin/sed "s#.*\(10\.[0-9]\+\.[0-9]\+\.[0-9]\+\)/[0-9].*#\1#" \
+| /usr/bin/head -1 | xargs host | sed "s/.* //;s/\.$//")
+
+# mount Ceph
+if [ ! -d "$CEPH_MOUNT" ]; then
+    mkdir "$CEPH_MOUNT"
+fi
+if ! grep -Fq "$CEPH_MOUNT" /etc/fstab; then
+    echo "mds11.grimer.stor,mds12.grimer.stor,mds13.grimer.stor:/external/vib $CEPH_MOUNT ceph secretfile=/mnt/vib.secret,name=vib,noatime 0 0" >> /etc/fstab
+fi
+mount "$CEPH_MOUNT"
 
 # Install and enable Docker service
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -24,20 +37,14 @@ fi
 # Get OpenNebula VM context variables
 source /tmp/one_env
 
-# Set hostname
-hostnamectl set-hostname --static $(/sbin/ip -4 -o addr show to 10.145.0.0/16 | \
-/bin/sed "s#.\(10\.[0-9]\+\.[0-9]\+\.[0-9]\+\)/[0-9].#\1#" | \
-/usr/bin/head -1 | xargs host | sed "s/.* //;s/\.$//")
-
-# mount Ceph
-if [ ! -d "$CEPH_MOUNT" ]; then
-    mkdir "$CEPH_MOUNT"
-fi
-mount -t ceph mds11.grimer.stor,mds12.grimer.stor,mds13.grimer.stor:/external/vib -o secretfile=/mnt/vib.secret,name=vib,noatime $CEPH_MOUNT
-
 # Setup the Docker Master or Worker
 if [ "$ROLE_NAME" == "Master" ]; then
     docker swarm init --advertise-addr "$ETH0_IP"
+    # Use local socket
+    echo '{"debug":true,"hosts":["tcp://'`hostname -f`':2375","unix:///var/run/docker.sock"]}' > /etc/docker/daemon.json
+    systemctl restart docker
+    # Drain Master from workers list
+    docker node update --availability drain `hostname -f`
 fi
 if [ "$ROLE_NAME" == "Worker" ]; then
     SWARM_TOKEN=$(su - docker -c "ssh -q -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking no' $MASTER_IP")
